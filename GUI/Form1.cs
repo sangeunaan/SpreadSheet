c9b4d73a96f266ca9493d2ec658cdc5ebef8321a
sangeunaan <redredred1210@gmail.com>
2023-11-12 20:24:17 +0900

    s

---
+++ GUI/Form1.cs
@@ -0,0 +1,19 @@
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
